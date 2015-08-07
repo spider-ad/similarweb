bbf30f4273691781240a600df461a6e8d1467cbd
Tales Mantovani <tales.mantovani@spider.ad>
2015-08-07 11:39:57 -0300

    Fix for new way to get page_views

--- lib/similarweb/estimated_visits.rb
+++ lib/similarweb/estimated_visits.rb
@@ -1,7 +1,9 @@
+require 'Date'
 module Similarweb
   module EstimatedVisits
     def estimated_visits(domain)
-      response = self.http_client.get "#{domain}/v1/EstimatedTraffic?Format=JSON&UserKey=#{self.api_key}"
+      date = Date.today.prev_month.strftime("%m-%Y")
+      response = self.http_client.get "#{domain}/v1/visits?start=#{date}&end=#{date}&Format=JSON&UserKey=#{self.api_key}"
       JSON(response.body)
     end
   end
