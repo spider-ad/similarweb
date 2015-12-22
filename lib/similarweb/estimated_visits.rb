effb9acd2b615e3f386ddf4f5c65e1694ee4b451
Diego Narducci <diego@css4html.com.br>
2015-12-22 16:13:32 -0200

    adding star and end date with one month by defautl if user dont passe this parameters

--- lib/similarweb/estimated_visits.rb
+++ lib/similarweb/estimated_visits.rb
@@ -4,8 +4,12 @@ module Similarweb
     def estimated_visits(domain, params = {})
       params.merge!({
         :Format => "JSON",
-        :Userkey => self.api_key
+        :Userkey => self.api_key,
       })
+
+      date = Date.today.prev_month.strftime("%m-%Y")
+      params[:Start] ||= date
+      params[:End] ||= date
 
       response = self.http_client.get "#{domain}/v1/visits?#{to_query(params)}"
       JSON(response.body)
