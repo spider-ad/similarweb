c5b55bd184966f873ded38e36401eeea451086b7
Tales Mantovani <talesmantovani100@gmail.com>
2016-03-04 16:08:50 -0300

    Url param as faraday way and remove format param

--- lib/similarweb/pageviews.rb
+++ lib/similarweb/pageviews.rb
@@ -3,15 +3,14 @@ module Similarweb
   module Pageviews
     def pageviews(domain, params = {})
       params.merge!({
-        :Format => "JSON",
         :Userkey => self.api_key,
       })
 
       date = Date.today.prev_month.strftime("%m-%Y")
-      params[:Start] ||= date
-      params[:End] ||= date
+      params[:start] ||= date
+      params[:end] ||= date
 
-      response = self.http_client.get "#{domain}/v1/pageviews?#{to_query(params)}"
+      response = self.http_client.get "#{domain}/v1/pageviews", params
       JSON(response.body)
     end
   end
